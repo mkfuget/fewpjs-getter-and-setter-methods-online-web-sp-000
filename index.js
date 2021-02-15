00662797fe9fd80b2d612f9e210b23795118e167
Michael Fuget <michaelfuget@gmail.com>
2021-02-15 01:01:31 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -17,4 +17,16 @@ class Circle
   {
     return this.radius*2*Math.PI
   }
+  set area(newArea)
+  {
+      this.radius = Math.sqrt(newArea/Math.PI)
+  }
+  set diameter(newDiameter)
+  {
+    this.radius = newDiameter/2;
+  }
+  set circumference(newCircumference)
+  {
+    this.radius = newCircumference/Math.PI;
+  }
 }
